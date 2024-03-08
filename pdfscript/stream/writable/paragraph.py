d8e1ed74790847a5bcdce00b4ac9cd345cc6a766
c7nw3r <christian.weber@leftshift.one>
2024-03-09 00:05:30 +0100

    bugfix paragraph writable

--- pdfscript/stream/writable/paragraph.py
+++ pdfscript/stream/writable/paragraph.py
@@ -2,7 +2,7 @@ from pdfscript.__spi__.pdf_context import PDFContext
 from pdfscript.__spi__.pdf_evaluation import SpaceSupplier
 from pdfscript.__spi__.pdf_writable import PDFEvaluation
 from pdfscript.__spi__.protocols import PDFListener, PDFOpset
-from pdfscript.__spi__.styles import TextStyle
+from pdfscript.__spi__.styles import TextStyle, LineStyle
 from pdfscript.__spi__.types import PDFPosition
 from pdfscript.stream.listener.noop_listener import NoOpListener
 from pdfscript.stream.writable.text import Text
@@ -17,9 +17,13 @@ class Paragraph(Text):
 
         def instr(ops: PDFOpset, pos: PDFPosition, get_space: SpaceSupplier):
             _, height = get_space(ops, pos)
+            one_line = height <= ops.get_height_of_text(".", self.style) + self.style.space_after
 
             super_instr(ops, pos, get_space)
             pos.x = pos.min_x
-            pos.y -= height
+            if one_line:
+                pos.y -= height
+
+            ops.draw_line(0, pos.y, 1000, pos.y, LineStyle("red"))
 
         return PDFEvaluation(super_space, instr)
